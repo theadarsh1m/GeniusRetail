a960f84d0e15297b60920151c5b29e773d8115fe
Adarsh Sachan <2k23.cs2312635@gmail.com>
2025-07-06 13:03:55 +0000

    chnage the website theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         ></link>
       </head>
